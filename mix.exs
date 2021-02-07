01999498bd78680a8a6e64e3be3a818b55a2a118
hailmarvin <mmwanzi76@gmail.com>
2021-02-07 20:44:15 +0300

    init on startup

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Chess.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {MyServer, []}
     ]
   end
 
