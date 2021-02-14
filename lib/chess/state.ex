9184c7dfe21bef38b4d39832c04bbc7bb78f1803
DevMarvin <mmwanzi76@gmail.com>
2021-02-14 15:23:36 +0300

    initial state

---
+++ lib/chess/state.ex
@@ -0,0 +1,11 @@
+defmodule Chess.State do
+    alias Chess.{Board, State}
+    @players [:white, :black]
+    @pieces [:pawn, :castle, :knight: :bishop, :king, :queen]
+
+    defstruct status: :initial,
+            turn: :white,
+            winner: nil,
+            board: Board.new_board(),
+            ui: nil
+end
